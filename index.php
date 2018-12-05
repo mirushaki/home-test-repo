4f5331e4194ca9d2a32e8e69f0a74d0345e97601
mirushaki <mirian.jinchvelashvili@gmail.com>
2018-12-06 01:59:41 +0400

    Creating branch 'phpsite', added index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+
+/**
+ * Created by PhpStorm.
+ * User: mirushaki
+ * Date: 12/6/18
+ * Time: 1:46 AM
+ */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>awesome website</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<?php
+echo "Hello, Git!";
+?>
+
+</body>
+</html>
